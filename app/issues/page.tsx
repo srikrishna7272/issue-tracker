ef1b9c8531c3707da41bbed69f505c8481c2b8fd
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-10 11:49:53 -0700

    built the new issue page.

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 
 const IssuesPage = () => {
-  return <Button>New Issue</Button>;
+  return (
+    <Button>
+      <Link href="/issues/new">New Issue</Link>
+    </Button>
+  );
 };
 
 export default IssuesPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <Theme>
           <NavBar />
-          <main>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
