2f71269a01ff35915e25542f08d3fb34b8f696e3
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-10 15:47:57 -0700

    show the issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet">
+        <Theme accentColor="violet">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
