eb98df9867402ead32c09e0c586f48cbec8d4e03
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-11 11:48:54 -0700

    add additional loading skeletons.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import delay from "delay";
 
 interface Props {
   params: { id: string };
@@ -15,6 +16,7 @@ const IssueDetailPage = async ({ params }: Props) => {
     where: { id: parseInt(params.id) },
   });
   if (!issue) notFound();
+
   return (
     <div>
       <Heading>{issue.title}</Heading>
