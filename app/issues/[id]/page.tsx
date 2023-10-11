cb52431311c39efb514666a06a9facbbe3c03cd6
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-11 15:43:30 -0700

    show a confirmation dailogue box.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -17,17 +17,19 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
-      <Box className="md:col-span-4">
-        <IssueDetails issue={issue} />
-      </Box>
-      <Box>
-        <Flex direction="column" gap="4">
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>
-    </Grid>
+    <>
+      <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+        <Box className="md:col-span-4">
+          <IssueDetails issue={issue} />
+        </Box>
+        <Box>
+          <Flex direction="column" gap="4">
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      </Grid>
+    </>
   );
 };
 
