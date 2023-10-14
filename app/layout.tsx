794c1555b1caf6d0534d6140c0284664efb28171
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-13 17:57:24 -0700

    set up react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
-import { Container, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Container, Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
-import "./globals.css";
-import "./theme-config.css";
 import AuthProvider from "./auth/Provider";
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+import QueryClientProvider from "./QueryClientProvider";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,14 +22,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme accentColor="violet">
-            <NavBar />
-            <main className="p-5">
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="violet">
+              <NavBar />
+              <main className="p-5">
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
