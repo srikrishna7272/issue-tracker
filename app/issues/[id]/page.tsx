96d074da31f9472430f1c4d3086ba27cef646a95
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-15 16:31:29 -0700

    added metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,37 +7,46 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 
 interface Props {
   params: { id: string };
 }
 
+const fetchUser = cache((issueId: number) => prisma.issue.findUnique({ where: { id: issueId } }));
+
 const IssueDetailPage = async ({ params }: Props) => {
   const session = await getServerSession(authOptions);
-  //  if (typeof params.id !== "number") notFound();
-  const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
-  });
+
+  const issue = await fetchUser(parseInt(params.id));
+
   if (!issue) notFound();
 
   return (
-    <>
-      <Grid columns={{ initial: "1", sm: "5" }} gap="5">
-        <Box className="md:col-span-4">
-          <IssueDetails issue={issue} />
+    <Grid columns={{ initial: "1", sm: "5" }} gap="5">
+      <Box className="md:col-span-4">
+        <IssueDetails issue={issue} />
+      </Box>
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
         </Box>
-        {session && (
-          <Box>
-            <Flex direction="column" gap="4">
-              <AssigneeSelect issue={issue} />
-              <EditIssueButton issueId={issue.id} />
-              <DeleteIssueButton issueId={issue.id} />
-            </Flex>
-          </Box>
-        )}
-      </Grid>
-    </>
+      )}
+    </Grid>
   );
 };
+
+export async function generateMetadata({ params }: Props) {
+  const issue = await fetchUser(parseInt(params.id));
+
+  return {
+    title: issue?.title,
+    description: "Details of issue " + issue?.id,
+  };
+}
 
 export default IssueDetailPage;
